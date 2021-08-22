04c66612eb9418fdea741e23d348c2190b7e4176
eunhyung-lee <88265446+eunhyung-lee@users.noreply.github.com>
2021-08-22 19:46:23 +0900

    learned function

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-alert(
-    "hi"
-);
+const player = {
+    name: "운형",
+    sayHello: function(otherPersonsName){
+        console.log("hello " + otherPersonsName);
+    }
+};
+console.log(player.name);
+player.sayHello("lee");
