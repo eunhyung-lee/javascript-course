4b2d70887c0609ebe1820f4dfd9db6aef1348927
eunhyung-lee <88265446+eunhyung-lee@users.noreply.github.com>
2021-08-31 22:04:15 +0900

    update files

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-const player = {
-    name: "운형",
-    sayHello: function(otherPersonsName){
-        console.log("hello " + otherPersonsName);
-    }
-};
-console.log(player.name);
-player.sayHello("lee");
+const age = parseInt(prompt("How old are you?"))
+
+console.log(age)
+
