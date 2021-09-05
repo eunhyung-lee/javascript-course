bcba5b8e8b884922dd2caba0aadb3df5c716ca61
eunhyung-lee <88265446+eunhyung-lee@users.noreply.github.com>
2021-09-05 22:03:02 +0900

    learn document.getElementById

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const age = parseInt(prompt("How old are you?"))
+// const age = parseInt(prompt("How old are you?"))
 
-if (isNaN(age)) {
-    console.log('please input a number')
-} else if (age < 18){
-    console.log("you are too young")
-} else if (age >=18 && age <=50){
-    console.log("you can drink")
-} else{
-    console.log("you should stop drinking")
-}
+// if (isNaN(age)) {
+//     console.log('please input a number')
+// } else if (age < 18){
+//     console.log("you are too young")
+// } else if (age >=18 && age <=50){
+//     console.log("you can drink")
+// } else{
+//     console.log("you should stop drinking")
+// }
+const title = document.getElementById("title");
+console.dir(title);
+title.innerText = "Got you!"
